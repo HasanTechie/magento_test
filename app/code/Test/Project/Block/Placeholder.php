9754d4e565052f480b73bfc478fab2356d8ee665
hasantechie <hasan.techie@gmail.com>
2024-04-16 13:20:02 +0500

    made image/title/paragraph changeable from backend, also loaded css via file

--- app/code/Test/Project/Block/Placeholder.php
+++ app/code/Test/Project/Block/Placeholder.php
@@ -23,4 +23,37 @@ class Placeholder extends Template
     {
         return $this->scopeConfig->getValue($path);
     }
+
+    public function isPlaceholderEnabled(){
+        return $this->getConfigValue('custom_section/custom_group/enable_placeholder') ?? false;
+    }
+
+    public function getTitle()
+    {
+        $value = $this->getConfigValue('custom_section/custom_group/title');
+        return $value !== null ? $value : '';
+    }
+
+    public function getParagraph()
+    {
+        $value = $this->getConfigValue('custom_section/custom_group/paragraph');
+        return $value !== null ? $value : '';
+    }
+
+    public function getImageUrl()
+    {
+        // Retrieve the base URL for media files
+        $mediaBaseUrl = $this->_storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA);
+
+        // Retrieve the path to the uploaded image from configuration
+        $imagePath = $this->scopeConfig->getValue('custom_section/custom_group/image_upload', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+
+        if($imagePath != null) {
+            $imageUrl = $mediaBaseUrl . 'placeholder/image/' . $imagePath;
+        }else{
+            $imageUrl = 'https://placehold.co/400';
+        }
+
+        return $imageUrl;
+    }
 }
